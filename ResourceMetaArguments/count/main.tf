2290f90b241bf95a6e0b2c8a10c9a785aad25dfb
bibincatchme <42738880+bibincatchme@users.noreply.github.com>
2021-03-29 23:36:32 +0530

    Create main.tf

---
+++ ResourceMetaArguments/count/main.tf
@@ -0,0 +1,20 @@
+  
+provider "aws" {
+	region = "us-east-1"
+}
+
+
+resource "aws_instance" "web" {
+	count         = 2
+	ami           = "ami-0533f2ba8a1995cf9"
+	instance_type = "t2.micro"
+
+	tags = {
+		Name = "Web Test ${count.index}"
+	}
+}
+
+#List of instances IP address.......
+output "instance" {
+  value = aws_instance.web[*].public_ip
+}
